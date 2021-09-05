99fdfffb00c407e477ffcc598d2548333d3e8a4b
Jose Castro <jacastro023@gmail.com>
2021-09-05 14:34:55 -0500

    Updated experied api

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ $(document).ready(function () {
             type: 'GET',
             url: 'https://www.googleapis.com/youtube/v3/search',
             data: {
-                key: 'AIzaSyCZs0g3qSFW3yteHvHDQ2ObfHG18qU9gzY',
+                key: 'AIzaSyD2kTjwcP4HhffwH5TooIC9J9P0rGxYjl4',
                 q: user1,
                 part: 'snippet',
                 maxResults: 5,
@@ -193,7 +193,7 @@ $(document).ready(function () {
             type: 'GET',
             url: 'https://www.googleapis.com/youtube/v3/search',
             data: {
-                key: 'AIzaSyCZs0g3qSFW3yteHvHDQ2ObfHG18qU9gzY',
+                key: 'AIzaSyD2kTjwcP4HhffwH5TooIC9J9P0rGxYjl4',
                 q: user2,
                 part: 'snippet',
                 maxResults: 10,
